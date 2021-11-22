2a58be0a5bbfa514b1010f7f87ffc0631886514e
zsmeszaros <zsolti0214@gmail.com>
2021-11-22 08:06:52 +0100

    feat cv and wiki

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header/>
+    <Body/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Body from '@/components/Body.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Body,
+    Footer
+  }
+}
+</script>
---
+++ src/views/Wiki.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header/>
+    <Body/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Body from '@/components/Body.vue'
+import Footer from '@/components/Footer.vue'
+import Test from 'raw-loader'
+// import dsd from './assets/test.txt'
+
+export default {
+  name: 'Wiki',
+  components: {
+    Header,
+    Body,
+    Footer
+  },
+  methods: {
+
+  },
+  created() {
+    // console.log("==== dsd", dsd)
+    console.log("==== Test", Test('./assets/test.txt'))
+  }
+}
+</script>
